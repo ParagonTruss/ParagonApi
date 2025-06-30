a95f231cf182eafe0b20254d078c9cbee7dad253
Nathan Bierema <nbierema@gmail.com>
2025-06-30 10:27:17 -0400

    12.0.0

--- ParagonApi/Models/Layouts/TrussEnvelope.cs
+++ ParagonApi/Models/Layouts/TrussEnvelope.cs
@@ -5,8 +5,11 @@ public class NewTrussEnvelope
     public required string Name { get; set; }
     public required Point2D LeftPoint { get; set; }
     public required Point2D RightPoint { get; set; }
+    public required double Elevation { get; set; }
     public required Justification Justification { get; set; }
     public required double Thickness { get; set; }
+    public required double LeftOverhang { get; set; }
+    public required double RightOverhang { get; set; }
     public BevelCut? LeftBevelCut { get; set; }
     public BevelCut? RightBevelCut { get; set; }
     public Guid? RoofContainerGuid { get; set; }
@@ -16,8 +19,6 @@ public class TrussEnvelope : NewTrussEnvelope
 {
     public Guid Guid { get; set; }
     public required Guid? ComponentDesignGuid { get; set; }
-    public required Point3D? BackLeftPoint { get; set; }
-    public required Point3D? BackRightPoint { get; set; }
 }
 
 public enum BevelCutType
