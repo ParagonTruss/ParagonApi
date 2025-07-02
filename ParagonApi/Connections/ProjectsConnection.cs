5fbbbf962bf103291c1dcf0373018ef735ca9327
Nathan Bierema <nbierema@gmail.com>
2025-07-02 11:37:08 -0400

    12.1.0

--- ParagonApi/Connections/ProjectsConnection.cs
+++ ParagonApi/Connections/ProjectsConnection.cs
@@ -4,6 +4,8 @@ public class ProjectsConnection(HttpClient designServiceClient)
 {
     private HttpClient Client { get; } = designServiceClient;
 
+    public Task<List<Project>> Get() => Client.Get<List<Project>>("api/public/projects");
+
     public Task<Project> Get(Guid guid) => Client.Get<Project>($"api/public/projects/{guid}");
 
     public Task<Project> Create(NewProject project) => Client.Post<NewProject, Project>("api/public/projects", project);
