d0468bb6469375d801f5488ddf4f1fe532d5d911
Scotty Hoelsema <sehcheese@gmail.com>
2026-03-20 17:17:01 -0400

    14.5.0

--- ParagonApi/Models/Trusses/Plate.cs
+++ ParagonApi/Models/Trusses/Plate.cs
@@ -8,6 +8,24 @@ public enum PlateOrientation
     TopBottom = 1,
 }
 
+public enum PlateGeometryType
+{
+    Rectangular = 0,
+    TPlate = 1,
+
+    /// <summary>
+    /// The left-handed bevel plate variant generally occurs on the left side of a truss viewed on a TDD. It is a
+    /// rectangle sliced at a 45-degree angle through the top right point when viewed unrotated.
+    /// </summary>
+    BevelPlateLeftHanded = 2,
+
+    /// <summary>
+    /// The right-handed bevel plate variant generally occurs on the right side of a truss viewed on a TDD. It is a
+    /// rectangle sliced at a 45-degree angle through the bottom right point when viewed unrotated.
+    /// </summary>
+    BevelPlateRightHanded = 3,
+}
+
 public class Plate
 {
     public required Guid Guid { get; set; }
@@ -27,4 +45,6 @@ public class Plate
     public required Direction3D SlotDirection { get; set; }
 
     public required Direction3D NormalDirection { get; set; }
+
+    public PlateGeometryType GeometryType { get; set; }
 }
